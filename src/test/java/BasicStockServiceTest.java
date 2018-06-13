85682aaaab28ecccc7ce8c5a399a6461fd6e5592
Stephan Stylianides <s_stylianides@yahoo.com>
2018-06-12 21:17:07 -0700

    added comments

--- src/test/java/BasicStockServiceTest.java
+++ src/test/java/BasicStockServiceTest.java
@@ -2,6 +2,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
 public class BasicStockServiceTest {
 
     private BasicStockService stockService;
@@ -62,7 +67,58 @@ public class BasicStockServiceTest {
 
     }
 
-    //TODO: write a positive and negative test for List<StockQuote> getQuote
+    /**
+     * test to return correct list
+     * @result will return the correct list
+     */
+    @Test
+    public void testGetCorrectStockList() throws ParseException {
+        String symbol = "QQQ";
+        String from = "1/11/1999";
+        String until = "12/1/2000";
+
+
+        SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar fromDate = Calendar.getInstance();
+        fromDate.setTime(parser.parse(from));
+        Calendar untilDate = Calendar.getInstance();
+        untilDate.setTime(parser.parse(until));
+
+
+        List<StockQuote> stockQuote = stockService.getQuote(symbol, fromDate, untilDate);
+        Assert.assertArrayEquals(stockQuote.toArray(), stockQuote.toArray());
+    }
+
+
+
+    /**
+     * negative test for empty symbol
+     * @result will return exception
+     */
+    @Test (expected = java.lang.IllegalArgumentException.class)
+    public void testThrowsIncorrectStockList() throws ParseException {
+        String symbol = "QQQ";
+        String from = "1/11/1998";
+        String until = "12/1/2001";
+
+        String symboltest = null;
+
+
+        SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy");
+
+
+        Calendar fromDate = Calendar.getInstance();
+        fromDate.setTime(parser.parse(from));
+        Calendar untilDate = Calendar.getInstance();
+        untilDate.setTime(parser.parse(until));
+
+
+        List<StockQuote> stockQuote = stockService.getQuote(symbol, fromDate, untilDate);
+        List<StockQuote> stockQuoteTest = stockService.getQuote(symboltest, fromDate, untilDate);
+
+
+        Assert.assertArrayEquals(stockQuote.toArray(), stockQuoteTest.toArray());
+    }
 
 
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,39 @@
 import org.junit.Test;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainTest {
 
     //TODO: write positive and negative Main Test
 
+    /**
+     * test main
+     * @result will return the correct Main
+     */
+    @Test
+    public void testMain() throws ParseException {
+        String symbol = "QQQ";
+        String from = "1/11/1998";
+        String until = "12/1/2001";
+
+        SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy");
+
+
+        Calendar fromDate = Calendar.getInstance();
+        fromDate.setTime(parser.parse(from));
+        Calendar untilDate = Calendar.getInstance();
+        untilDate.setTime(parser.parse(until));
+
+        Main.main(new String[] {symbol, from, until});
+    }
+
+
+    /**
+     * negative test for empty Main
+     * @result will return exception
+     */
     @Test(expected = NullPointerException.class)
     public void testMainNegative() throws ParseException {
         Main.main(null);
