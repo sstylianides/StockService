e2b85e23eb9a6e780757945cb3f3df3179a4e77e
Stephan Stylianides <s_stylianides@yahoo.com>
2018-07-26 16:08:27 -0700

    configure yml file

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,31 +3,10 @@ import services.StockServiceException;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class MainTest {
 
-    /**
-     * test main
-     * @result will return the correct Main
-     */
-    @Test
-    public void testMain() throws ParseException, StockServiceException, IOException {
-        String symbol = "VNET";
-        String from = "02/10/2015";
-        String until = "02/10/2015";
-
-        SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy");
-
-
-        Calendar fromDate = Calendar.getInstance();
-        fromDate.setTime(parser.parse(from));
-        Calendar untilDate = Calendar.getInstance();
-        untilDate.setTime(parser.parse(until));
-
-        StockQuoteApp.main(new String[] {symbol, from, until});
-    }
+   
 
 
     /**
